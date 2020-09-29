d6939b316153a86da83025e9f0c2b63b334297fa
Ajish V Nair <ajishvnair55@gmail.com>
2020-09-30 01:08:04 +0530

    feat(apps): map tunes state and reducers to home container

--- app/containers/HomeContainer/index.js
+++ app/containers/HomeContainer/index.js
@@ -1,22 +1,35 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
+import React, { useState, memo } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { injectIntl } from 'react-intl';
 import { Input } from 'antd';
-import { getTunes } from '@services/iTunesApi';
+import PropTypes from 'prop-types';
+import { createStructuredSelector } from 'reselect';
+import { useInjectSaga } from 'utils/injectSaga';
+// import { getTunes } from '@services/iTunesApi';
+import { selectHomeContainer, selectTunesData, selectTunesError, selectKeyword } from './selectors';
+import { homeContainerCreators } from './reducer';
+import saga from './saga';
 
 const { Search } = Input;
 
-export default function() {
+export function HomeContainer({
+    dispatchTunes,
+    dispatchClearTunes,
+    intl,
+    tunesData = {},
+    tunesError = null,
+    keyword,
+    maxwidth,
+    padding
+}) {
+    useInjectSaga({ key: 'homeContainer', saga });
     const [searchText, setSearchText] = useState('');
     const [data, setData] = useState([]);
 
     const handleSearch = () => {
-        getTunes(searchText)
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                console.log(err);
-            });
+        dispatchTunes(searchText);
     };
     return (
         <div>
@@ -25,3 +38,52 @@ export default function() {
         </div>
     );
 }
+
+HomeContainer.propTypes = {
+    dispatchTunes: PropTypes.func,
+    dispatchClearTunes: PropTypes.func,
+    intl: PropTypes.object,
+    tunesData: PropTypes.shape({
+        totalCount: PropTypes.number,
+        incompleteResults: PropTypes.bool,
+        items: PropTypes.array
+    }),
+    tunesError: PropTypes.object,
+    keyword: PropTypes.string,
+    history: PropTypes.object,
+    maxwidth: PropTypes.number,
+    padding: PropTypes.number
+};
+
+HomeContainer.defaultProps = {
+    maxwidth: 500,
+    padding: 20
+};
+
+const mapStateToProps = createStructuredSelector({
+    homeContainer: selectHomeContainer(),
+    tunesData: selectTunesData(),
+    tunesError: selectTunesError(),
+    keyword: selectKeyword()
+});
+
+function mapDispatchToProps(dispatch) {
+    const { requestGetTunes, clearTunes } = homeContainerCreators;
+    return {
+        dispatchTunes: repoName => dispatch(requestGetTunes(repoName)),
+        dispatchClearTunes: () => dispatch(clearTunes())
+    };
+}
+
+const withConnect = connect(
+    mapStateToProps,
+    mapDispatchToProps
+);
+
+export default compose(
+    injectIntl,
+    withConnect,
+    memo
+)(HomeContainer);
+
+export const HomeContainerTest = compose(injectIntl)(HomeContainer);
