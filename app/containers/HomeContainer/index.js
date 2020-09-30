d50b066e2200e6f0b2d075399aa256df5d59f03a
Ajish V Nair <ajishvnair55@gmail.com>
2020-09-30 12:26:59 +0530

    feat(apps): added search box style

--- app/containers/HomeContainer/index.js
+++ app/containers/HomeContainer/index.js
@@ -3,18 +3,38 @@ import React, { useState, memo } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { injectIntl } from 'react-intl';
-import { Input } from 'antd';
+import { Input, Card } from 'antd';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { createStructuredSelector } from 'reselect';
 import { useInjectSaga } from 'utils/injectSaga';
 import debounce from 'lodash/debounce';
 import isEmpty from 'lodash/isEmpty';
+import T from '@components/T';
 // import { getTunes } from '@services/iTunesApi';
 import { selectHomeContainer, selectTunesData, selectTunesError, selectKeyword } from './selectors';
 import { homeContainerCreators } from './reducer';
 import saga from './saga';
 
 const { Search } = Input;
+
+const Container = styled.div`
+    && {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+`;
+const CustomCard = styled(Card)`
+    && {
+        margin-top: -10px;
+        max-width: ${props => props.maxwidth};
+        min-width: 400px;
+        color: ${props => props.color};
+        ${props => props.color && `color: ${props.color}`};
+    }
+`;
 
 export function HomeContainer({
     dispatchTunes,
@@ -39,14 +59,17 @@ export function HomeContainer({
 
     const debouncedHandleSerach = debounce(handleSearch, 200);
     return (
-        <div>
-            <Search
-                value={keyword}
-                onChange={e => debouncedHandleSerach(e.target.value)}
-                onSearch={searchText => debouncedHandleSerach(searchText)}
-            />
+        <Container>
+            <CustomCard maxwidth={maxwidth}>
+                <Search
+                    placeholder={intl.formatMessage({ id: 'search_artist' })}
+                    value={keyword}
+                    onChange={e => debouncedHandleSerach(e.target.value)}
+                    onSearch={searchText => debouncedHandleSerach(searchText)}
+                />
+            </CustomCard>
             {JSON.stringify(tunesData)}
-        </div>
+        </Container>
     );
 }
 
@@ -63,11 +86,13 @@ HomeContainer.propTypes = {
     keyword: PropTypes.string,
     history: PropTypes.object,
     maxwidth: PropTypes.number,
+    width: PropTypes.number,
     padding: PropTypes.number
 };
 
 HomeContainer.defaultProps = {
-    maxwidth: 500,
+    maxwidth: 200,
+    width: 800,
     padding: 20
 };
 
